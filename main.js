7f70eef750dc373736529dc74119d0973a83c071
EC2 Default User <ec2-user@ip-172-31-53-37.ec2.internal>
2020-07-12 17:19:39 +0000

    表を微調整

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ const showResult = result => {
                         <td>${item.tel}</td>
                         <td><a href="${item.url}">pc版サイト</a></td>
                         <td><a href="${item.url_mobile}">スマホ版サイト</a></td>
-                        <td>${item.access.walk}</td>
+                        <td>${item.access.station}${item.access.walk}分</td>
                         <td>${item.code.prefname}</td>
                         <td>${item.pr.pr_short}</td>
-                        <td>${item.image_url.shop_image1}</td>
-                        <td>${item.image_url.shop_image2}</td>
+                        <td><img src="${item.image_url.shop_image1}"></td>
+                        <td><img src="${item.image_url.shop_image2}"></td>
                         <td>${item.e_money}</td>
                     </tr>
                 `);
@@ -28,8 +28,11 @@ $(function() {
     navigator.geolocation.getCurrentPosition((position) => {
 
         // 緯度経度の取得
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
+        // pcの現在位置がおかしいため固定する
+        // lat = position.coords.latitude;
+        // lng = position.coords.longitude;
+        lat=35.02725763144587;
+        lng=137.02609432481168;
 
         console.log(lat, lng);
 
@@ -38,7 +41,7 @@ $(function() {
             latitude: lat,
             longitude: lng,
             range: 5,
-            freeword: "ラーメン"
+            hit_per_page:20
         }
 
 
